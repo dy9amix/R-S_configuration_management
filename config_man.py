a6f1220fb4b6365f81367bc1be23da89747be267
root <segunorodeji@gmail.com>
2020-05-01 23:09:48 +0100

    more pylint changes

--- config_man.py
+++ config_man.py
@@ -1,15 +1,12 @@
-""""This script automates network configs using nornir"""
+""""This is a simple nornir automation script"""
 from nornir import Initnornir #pylint: disable=import-error
 from nornir.plugins.tasks.networking import napalm_get #pylint: disable=import-error
 from nornir.plugins.functions.text import print_results #pylint: disable=import-error
 
-def main():
-	nr = Initnornir(config_file="config.yaml")
+NR = Initnornir(config_file="config.yaml")
 
-	result = nr.run(
-				napalm_get,
-				getters=['get_facts'])
+RESULTS = NR.run(
+			 napalm_get,
+			 getters=['get_facts'])
 
-	print_results(result)
-
-main()
+print_results(RESULTS)
