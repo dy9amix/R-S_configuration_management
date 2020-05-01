9a572fadf868310a7681c953508879751e237c6f
root <segunorodeji@gmail.com>
2020-05-01 22:56:24 +0100

    made some pylint changes

--- config_man.py
+++ config_man.py
@@ -5,7 +5,7 @@ from nornir.plugins.functions.text import print_results #pylint: disable=import-
 nr = Initnornir(config_file="config.yaml")
 
 result = nr.run(
-			napalm_get,
-			getters=['get_facts'])
+			 napalm_get,
+			 getters=['get_facts'])
 
 print_results(result)
