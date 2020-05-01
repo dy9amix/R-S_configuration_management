2668efe4872d8745cc85792a26c7cc657752160f
dy9amix <32060683+dy9amix@users.noreply.github.com>
2020-05-01 23:23:22 +0100

    Update config_man.py

--- config_man.py
+++ config_man.py
@@ -3,7 +3,7 @@ from nornir import InitNornir #pylint: disable=import-error
 from nornir.plugins.tasks.networking import napalm_get #pylint: disable=import-error
 from nornir.plugins.functions.text import print_results #pylint: disable=import-error
 
-NR = Initnornir(config_file="config.yaml")
+NR = InitNornir(config_file="config.yaml")
 
 RESULTS = NR.run(
 			 napalm_get,
