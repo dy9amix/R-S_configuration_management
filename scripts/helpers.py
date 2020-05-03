7baa82a3c6169d9ac87e232203d8f74ee9bfe917
root <segunorodeji@gmail.com>
2020-05-03 11:33:02 +0100

    No syntax error found

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,4 +1,12 @@
-from secrets import creds
+creds = {
+    "SW-1": {"username": "admin", "password": "admin"},
+    "SW-2": {"username": "admin", "password": "admin"},
+    "SW-3": {"username": "admin", "password": "admin"},
+    "SW-4": {"username": "admin", "password": "admin"},
+    "SW-5": {"username": "admin", "password": "admin"},
+    "SW-6": {"username": "admin", "password": "admin"}
+    }
+
 
 def adapt_user_password(host):
     host.username = creds[f"{host}"]["username"]
--- scripts/secrets.py
+++
@@ -1,7 +0,0 @@
-creds = {
-    "SW-1": {"username": "admin", "password": "admin"},
-    "SW-2": {"username": "admin", "password": "admin"},
-    "SW-3": {"username": "admin", "password": "admin"},
-    "SW-4": {"username": "admin", "password": "admin"},
-    "SW-5": {"username": "admin", "password": "admin"},
-    "SW-6": {"username": "admin", "password": "admin"},
