2aa1c673bafbe3d6fb0832bb54dfc868098ed7d0
root <segunorodeji@gmail.com>
2020-05-09 20:09:16 +0100

    changed netbox slug

--- config_man.py
+++ config_man.py
@@ -1,5 +1,5 @@
 #!./venv/bin/python
-""""This is a simple nornir automation script."""
+""""This is a simple nornir automation script. It is"""
 from nornir.plugins.tasks import networking #pylint: disable=import-error
 from nornir.plugins.functions.text import print_result #pylint: disable=import-error
 from nornir.plugins.tasks.files import write_file #pylint: disable=import-error
