a5ddac72b22d7fc33b92ee6da9d787ba069620e0
root <segunorodeji@gmail.com>
2020-05-09 16:27:49 +0100

    Update

--- config_man.py
+++ config_man.py
@@ -1,4 +1,4 @@
-""""This is a simple nornir automation script"""
+""""This is a simple nornir automation script. It is"""
 from nornir import InitNornir #pylint: disable=import-error
 from nornir.plugins.tasks.networking import napalm_get #pylint: disable=import-error
 from nornir.plugins.functions.text import print_result #pylint: disable=import-error
