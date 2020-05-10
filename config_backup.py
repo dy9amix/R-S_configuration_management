417b094f236ee4a06e7d40168fbe469e8e33aa5f
root <segunorodeji@gmail.com>
2020-05-10 20:55:35 +0100

    Load configs to topology trial one

---
+++ config_backup.py
@@ -0,0 +1,28 @@
+#!./venv/bin/python
+"""This script takes a backup of all the devices on the topology"""
+from nornir.plugins.tasks import networking #pylint: disable=import-error
+from nornir.plugins.functions.text import print_result #pylint: disable=import-error
+from nornir.plugins.tasks.files import write_file #pylint: disable=import-error
+from nornir import InitNornir #pylint: disable=import-error
+
+BACKUP_PATH = "./data/configs"
+
+
+def backup_config(task, path): # pylint: disable=missing-function-docstring
+    r_var = task.run(task=networking.napalm_get, getters=["config"])
+    task.run(
+        task=write_file,
+        content=r_var.result["config"]["running"],
+        filename=f"{path}/{task.host}.txt",
+    )
+
+
+NR = InitNornir(config_file="./config.yaml")
+
+DEVICES = NR.filter(role="switch")
+
+RESULT = DEVICES.run(
+    name="Backup Device configurations", path=BACKUP_PATH, task=backup_config
+)
+
+print_result(RESULT, vars=["stdout"])
--- config_man.py
+++ config_man.py
@@ -1,28 +1,86 @@
 #!./venv/bin/python
-""""This is a simple nornir automation script. It is"""
+""""This is a script automates configuration managaement for the GNS3 production network defined """
 from nornir.plugins.tasks import networking #pylint: disable=import-error
 from nornir.plugins.functions.text import print_result #pylint: disable=import-error
-from nornir.plugins.tasks.files import write_file #pylint: disable=import-error
 from nornir import InitNornir #pylint: disable=import-error
 
-BACKUP_PATH = "./data/configs"
 
-def backup_config(task, path): # pylint: disable=missing-function-docstring
+def sw_a_config(task): # pylint: disable=missing-function-docstring
 
-    res_view = task.run(task=networking.napalm_get, getters=["config"])
-    task.run(
-        task=write_file,
-        content=res_view.result["config"]["running"],
-        filename=f"{path}/{task.host}.txt",
-    )
+    res_view = task.run(task=networking.napalm_configure,
+                        dry_run=True,
+                        filename="./IOS_config/SW-1.conf"
+                        )
+    print_result(res_view.result)
+
+def sw_b_config(task): # pylint: disable=missing-function-docstring
+
+    res_view = task.run(task=networking.napalm_configure,
+                        dry_run=True,
+                        filename="./IOS_config/SW-2.conf"
+                        )
+    print_result(res_view.result)
+
+def sw_c_config(task): # pylint: disable=missing-function-docstring
+
+    res_view = task.run(task=networking.napalm_configure,
+                        dry_run=True,
+                        filename="./IOS_config/SW-3.conf"
+                        )
+    print_result(res_view.result)
+
+def sw_d_config(task): # pylint: disable=missing-function-docstring
+
+    res_view = task.run(task=networking.napalm_configure,
+                        dry_run=True,
+                        filename="./IOS_config/SW-4.conf"
+                        )
+    print_result(res_view.result)
+
+def sw_e_config(task): # pylint: disable=missing-function-docstring
+
+    res_view = task.run(task=networking.napalm_configure,
+                        dry_run=True,
+                        filename="./IOS_config/SW-5.conf"
+                        )
+    print_result(res_view.result)
 
 
 NR = InitNornir(config_file="./config.yaml")
 
-DEVICES = NR.filter(role="leaf")
+SW_1 = NR.filter(name="SW-1")
+SW_2 = NR.filter(name="SW-2")
+SW_3 = NR.filter(name="SW-3")
+SW_4 = NR.filter(name="SW-4")
+SW_5 = NR.filter(name="SW-5")
 
-RESULT = DEVICES.run(
-    name="Backup Device configurations", path=BACKUP_PATH, task=backup_config
+
+RESULT_SW_1 = SW_1.run(
+    name="Configure Switch SW-1", task=sw_a_config
 )
 
-print_result(RESULT)
+RESULT_SW_2 = SW_2.run(
+    name="Configure Switch SW-2", task=sw_b_config
+)
+
+RESULT_SW_3 = SW_3.run(
+    name="Configure Switch SW-3", task=sw_c_config
+)
+
+RESULT_SW_4 = SW_4.run(
+    name="Configure Switch SW-4", task=sw_d_config
+)
+
+RESULT_SW_5 = SW_5.run(
+    name="Configure Switch SW-5", task=sw_e_config
+)
+
+print_result(RESULT_SW_1)
+print('/n')
+print_result(RESULT_SW_2)
+print('/n')
+print_result(RESULT_SW_3)
+print('/n')
+print_result(RESULT_SW_4)
+print('/n')
+print_result(RESULT_SW_5)
